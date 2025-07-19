911ed30caf3b041187e7e241b9b39f1e32db8528
Daniel Kandie <danielkandie14@gmail.com>
2025-07-19 20:22:26 +0300

    Feat: Used Card components in landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Card from "@/components/Card";
 
-const Landing: React.FC =  () => {
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-4">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+      
+      {/* Use Card Component */}
+      <Card />
+      <Card />
+      <Card />
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
